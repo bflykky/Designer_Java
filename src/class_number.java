f76eb9925fa4475cbf7c06b664e4981c9cc08f2d
bfly_Desktop <kkyxw@soongsil.ac.kr>
2021-03-15 22:38:52 +0900

    클래스 파일의 개수와 같은 생성 결과를 보기 위한 파일.

---
+++ src/class_number.java
@@ -0,0 +1,20 @@
+public class class_number {
+    public static void main(String[] args) {
+        System.out.println("궁금한 거 풀기");
+        //주석
+        //주석
+    }
+        class B {
+            //주석
+        }
+
+
+}
+
+class C {
+    //aaa
+    class D {
+        //bbb
+
+    }
+}
