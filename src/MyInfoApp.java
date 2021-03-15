54dfd44362d8856d4e0a563ea49b592a4fb3bc24
bfly_Desktop <kkyxw@soongsil.ac.kr>
2021-03-15 22:20:51 +0900

    1번째 푸시? 커밋? 허허 아니면 둘다?

---
+++ src/MyInfoApp.java
@@ -0,0 +1,7 @@
+public class MyInfoApp {
+    public static void main(String[] args) {
+        System.out.println("Name : Kim Kang Yeon");
+        System.out.println("Age : 20 years old");
+        System.out.println("Major : Department of Software");
+    }
+}
