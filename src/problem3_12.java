61d363202629f2aba8429cd943faa261a5439325
bfly_Desktop <kkyxw@soongsil.ac.kr>
2021-04-18 15:08:13 +0900

    3_12

--- src/problem3_12.java
+++ src/problem3_12.java
@@ -1,2 +1,18 @@
 public class problem3_12 {
+    public static void main(String[] args) {
+        int[] arr = new int[args.length];
+        int sum = 0;
+        int i = 0;
+        while(i < args.length) {
+            try {
+                arr[i] = Integer.parseInt(args[i]);
+                sum += arr[i];
+                i++;
+            } catch (NumberFormatException e) {
+                System.out.println(args[i] + "는 정수로 변환할 수 없습니다.");
+                i++;
+            }
+        }
+        System.out.println(sum);
+    }
 }
