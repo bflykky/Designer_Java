08bf276afbc62a09a5f97f5dad65ed4d1891c1b0
bfly_Desktop <kkyxw@soongsil.ac.kr>
2021-05-08 15:41:03 +0900

    problem4_12 문제 풀이 소스코드 개선점 : S, A, B 좌석을 출력할 때 "s>> aaa aaa bbb ccc" 처럼 맨 처음 부분에 출력하는 좌석의 이름을 나타내어야 하는데, 클래스를 만들어 메소드를 사용함에 따라 좌석의 이름을 출력하기가 어렵게 됨. reflection을 이용하여 필드명을 받아오는 방법 외에 다른 방법 생각중

--- src/problem4_12.java
+++ src/problem4_12.java
@@ -1,2 +1,110 @@
+import java.util.Scanner;
+
+class Seat {
+    static String[] seatS = {"---", "---", "---", "---", "---", "---", "---", "---", "---", "---"};
+    static String[] seatA = {"---", "---", "---", "---", "---", "---", "---", "---", "---", "---"};
+    static String[] seatB = {"---", "---", "---", "---", "---", "---", "---", "---", "---", "---"};
+
+    static void printSeat(String[] seat) {
+        System.out.print("미정" + ">>");
+        for (String index : seat)
+            System.out.print(" " + index);
+        System.out.println();
+    }
+
+    static void reserve(String[] seat) {
+        Scanner s = new Scanner(System.in);
+        do {
+            System.out.print("예약자 이름>> ");
+            String inputName = s.next();
+            System.out.print("예약 좌석 번호>> ");
+            int inputSeatNum = s.nextInt();
+            try {
+                seat[inputSeatNum - 1] = inputName;
+                break;
+            } catch (ArrayIndexOutOfBoundsException e) {
+                System.out.println("좌석 번호가 올바르지 않습니다. 다시 시도해 주세요.");
+                continue;
+            }
+        } while (true);
+    }
+
+    static void cancelReservation(String[] seat) {
+        Scanner s = new Scanner(System.in);
+        do {
+            System.out.print("이름>> ");
+            String inputName = s.next();
+            int count = 0;
+            for (int i = 0; i < seat.length; i++) {
+               if (seat[i].equals(inputName)) {
+                   seat[i] = "---";
+                   break;
+               }
+               else
+                   count++;
+            }
+            if (count == 10)
+                System.out.println("입력하신 이름으로 해당된 예약자가 없습니다. 다시 시도해 주세요.");
+            else
+                break;
+        } while(true);
+    }
+}
+
 public class problem4_12 {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        System.out.println("명품콘서트홀 예약 시스템입니다.");
+        int inputNum;
+
+        do {
+            System.out.print("예약: 1, 조회: 2, 취소: 3, 끝내기: 4 >> ");
+            inputNum = s.nextInt();
+
+            if (inputNum == 1) {
+                System.out.print("좌석구분 S(1), A(2), B(3)>> ");
+                inputNum = s.nextInt();
+                if (inputNum == 1) {
+                    Seat.printSeat(Seat.seatS);
+                    Seat.reserve(Seat.seatS);
+                }
+                else if (inputNum == 2) {
+                    Seat.printSeat(Seat.seatA);
+                    Seat.reserve(Seat.seatA);
+                }
+                else if (inputNum == 3) {
+                    Seat.printSeat(Seat.seatB);
+                    Seat.reserve(Seat.seatB);
+                }
+                else
+                    System.out.println("좌석을 잘못 입력하였습니다. 처음부터 다시 시도해 주세요.");
+
+            }
+            else if (inputNum == 2) {
+                Seat.printSeat(Seat.seatS);
+                Seat.printSeat(Seat.seatA);
+                Seat.printSeat(Seat.seatB);
+                System.out.println("<<<조회를 완료하였습니다.>>>");
+            }
+            else if (inputNum == 3) {
+                System.out.print("좌석 S: 1, A: 2, B: 3 >> ");
+                inputNum = s.nextInt();
+                if (inputNum == 1)
+                    Seat.cancelReservation(Seat.seatS);
+                else if (inputNum == 2)
+                    Seat.cancelReservation(Seat.seatA);
+                else if (inputNum == 3)
+                    Seat.cancelReservation(Seat.seatB);
+                else
+                    System.out.println("좌석을 잘못 입력하였습니다. 처음부터 다시 시도해 주세요.");
+            }
+            else if (inputNum == 4)
+                break;
+            else {
+                System.out.println("없는 메뉴입니다. 다시 시도해 주세요.");
+                continue;
+            }
+        } while (true);
+        s.close();
+    }
 }
